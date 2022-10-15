4d5971f5db49ad8133c789ec5c416048c1742ef9
ericnandaferdian <115437986+ericnandaferdian@users.noreply.github.com>
2022-10-15 16:13:02 +0800

    Delete C directory

--- C/PRAK002-2210817210010-Eric Nanda ferdian.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("Selamat Pagi,Eric Nanda Ferdian\n");
-    printf("Selamat Siang,Eric Nanda Ferdian\n");
-    printf("Selamat Malam,Eric Nanda Ferdian\n");
-}
