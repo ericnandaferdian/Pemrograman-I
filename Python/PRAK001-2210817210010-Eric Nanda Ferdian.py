40b27a4115c260c8edb587c62a3f539c7bf5edc7
ericnandaferdian <115437986+ericnandaferdian@users.noreply.github.com>
2022-10-15 16:15:47 +0800

    Delete Python directory

--- Python/PRAK001-2210817210010-Eric Nanda Ferdian.py
+++
@@ -1,1 +0,0 @@
-print("Saya Calon Programmer NO. 1")
--- Python/PRAK002-2210817210010-Eric Nanda Ferdian.py
+++
@@ -1,3 +0,0 @@
-print("Selamat Pagi, Eric Nanda Ferdian")
-print("Selamat Siang, Eric Nanda Ferdian")
-print("Selamat Malam, Eric Nanda Ferdian")
